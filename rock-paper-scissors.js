76c2331aced39b392006cf013091f4728093eda3
MyRepo21 <student2020@nym.hush.com>
2021-06-30 09:19:43 -0700

    Updates

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,4 @@
+/*
 let computerChoice;
 
 const gameBtns = document.querySelectorAll("[type=image]");
@@ -35,3 +36,51 @@ function determineWinner(userChoice, computerChoice) {
         results.textContent = "Tie";
     }
 }
+*/
+
+let computerChoice;
+const gameBtns = document.querySelectorAll("[type=image]");
+gameBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const userChoice = e.target.textContent.toLowerCase();
+    getComputerChoice();
+    determineWinner(userChoice, computerChoice);
+    newGame(results);
+  });
+});
+
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+  alert(`Computer plays ${computerChoice}`);
+}
+
+function determineWinner(userChoice, computerChoice) {
+  let results = document.getElementById("results");
+
+  if (
+    (userChoice === "rock" && computerChoice === "scissors") ||
+    (userChoice === "paper" && computerChoice === "rock") ||
+    (userChoice === "scissors" && computerChoice === "paper")
+  ) {
+    results.textContent = "You won";
+  } else if (
+    (computerChoice === "rock" && userChoice === "scissors") ||
+    (computerChoice === "paper" && userChoice === "rock") ||
+    (computerChoice === "scissors" && userChoice === "paper")
+  ) {
+    results.textContent = "Computer won";
+  } else {
+    results.textContent = "Tie";
+  }
+}
+
+function newGame() {
+    let results = document.getElementById("results");
+    const newGameBtn = document.getElementById("new-game-btn");
+    newGameBtn.addEventListener("click", (e) => {
+    results.textContent = "";
+    });
+}
+
