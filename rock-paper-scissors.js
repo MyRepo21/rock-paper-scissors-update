755ed39da2eeaa213aaf0e11c24fe47f5bfa388e
MyRepo21 <student2020@nym.hush.com>
2021-07-02 08:53:42 -0700

    Updates

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,86 +1,50 @@
-/*
 let computerChoice;
-
+let playerChoice;
 const gameBtns = document.querySelectorAll("[type=image]");
+
 gameBtns.forEach((btn) => {
     btn.addEventListener("click", (e) => {
-        const userChoice = e.target.textContent.toLowerCase();
+        const userChoice = e.target.textContent .toLowerCase();
+        playerChoice = btn.value
         getComputerChoice();
-        determineWinner(userChoice, computerChoice);
+        determineWinner(playerChoice, computerChoice);
+        newGame();
     });
 });
 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     computerChoice = choices[Math.floor(Math.random() * choices.length)];
-
+    alert("Player plays " + playerChoice);
     alert(`Computer plays ${computerChoice}`);
 }
 
-function determineWinner(userChoice, computerChoice) {
+function determineWinner(playerChoice, computerChoice) {
     let results = document.getElementById("results");
-
+  
     if (
-        (userChoice === "rock" && computerChoice === "scissors") || 
-        (userChoice === "paper" && computerChoice === "rock") ||
-        (userChoice === "scissors" && computerChoice === "paper")
+        (playerChoice === "rock" && computerChoice === "scissors") || 
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
     ) { 
-        results.textContent = "You won";
+        results.textContent = "Player won";
     }
     else if (        
-        (computerChoice === "rock" && userChoice === "scissors") || 
-        (computerChoice === "paper" && userChoice === "rock") ||
-        (computerChoice === "scissors" && userChoice === "paper")
-    ) { results.textContent = "Computer won";
+        (computerChoice === "rock" && playerChoice === "scissors") || 
+        (computerChoice === "paper" && playerChoice === "rock") ||
+        (computerChoice === "scissors" && playerChoice === "paper")
+    ) { 
+      results.textContent = "Computer won";
     } else {
         results.textContent = "Tie";
     }
 }
-*/
-
-let computerChoice;
-const gameBtns = document.querySelectorAll("[type=image]");
-gameBtns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const userChoice = e.target.textContent.toLowerCase();
-    getComputerChoice();
-    determineWinner(userChoice, computerChoice);
-    newGame(results);
-  });
-});
-
-function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"];
-  computerChoice = choices[Math.floor(Math.random() * choices.length)];
-
-  alert(`Computer plays ${computerChoice}`);
-}
-
-function determineWinner(userChoice, computerChoice) {
-  let results = document.getElementById("results");
-
-  if (
-    (userChoice === "rock" && computerChoice === "scissors") ||
-    (userChoice === "paper" && computerChoice === "rock") ||
-    (userChoice === "scissors" && computerChoice === "paper")
-  ) {
-    results.textContent = "You won";
-  } else if (
-    (computerChoice === "rock" && userChoice === "scissors") ||
-    (computerChoice === "paper" && userChoice === "rock") ||
-    (computerChoice === "scissors" && userChoice === "paper")
-  ) {
-    results.textContent = "Computer won";
-  } else {
-    results.textContent = "Tie";
-  }
-}
 
 function newGame() {
-    let results = document.getElementById("results");
-    const newGameBtn = document.getElementById("new-game-btn");
-    newGameBtn.addEventListener("click", (e) => {
-    results.textContent = "";
-    });
+  let results = document.getElementById("results");
+  const newGameBtn = document.getElementById("new-game-btn");
+  newGameBtn.addEventListener("click", (e) => {
+  results.textContent = "";
+  });
 }
 
